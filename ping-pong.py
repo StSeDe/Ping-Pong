b08d1fe11f23406431df7b82fd2a9ce04473defc
SSD <131570455+StSeDe@users.noreply.github.com>
2025-05-04 13:04:49 +0500

    Update ping-pong.py
    добавлены правила

--- ping-pong.py
+++ ping-pong.py
@@ -61,7 +61,21 @@ while game:
         racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+                   #если мяч достигает границ экрана, меняем направление его движения
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
 
+        #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
         racket1.reset()
         racket2.reset()
         ball.reset()
